50b2a2d99f164f60140c67f925d6f3df4062db2a
Kt Gunn <ktgunn@hotmail.com>
2020-02-13 18:47:24 -0500

    slights mods to kdtree.h

--- cluster.d/kdtree.h
+++ cluster.d/kdtree.h
@@ -8,18 +8,18 @@
 
 // Structure to represent node of kd tree
 struct Node {
-
+  
   // Contents of the node, coords & ID
   std::vector<float> point;
   int id;
-
+  
   // Pointer to the attached nodes
   Node* left;
   Node* right;
   
   // Node constructor
   Node (std::vector<float> arr, int setId)
-     :	point(arr), id(setId), left(NULL), right(NULL) {}
+  :	point(arr), id(setId), left(NULL), right(NULL) {}
 };
 
 struct KdTree {
@@ -39,30 +39,30 @@ struct KdTree {
     return;
   }
   
-
+  
   // This is s recursive function
   void insertHelper (Node** node, uint depth, std::vector<float> point, int id) {
-
+    
     if (*node == nullptr) {
-
+      
       // Create and assign the new node
       *node = new Node (point, id);
       
     } else {
-
+      
       // Compare based on x[0]/y[1]
       uint Zero1 = depth % 2;
-
+      
       if (point[Zero1] < ((*node)->point[Zero1])) {
-	// Go left
-	insertHelper (&((*node)->left), 1+depth, point, id);
-
+        // Go left
+        insertHelper (&((*node)->left), 1+depth, point, id);
+        
       } else {
-	// Go right
-	insertHelper (&((*node)->right), 1+depth, point, id);
+        // Go right
+        insertHelper (&((*node)->right), 1+depth, point, id);
       }
     }
-
+    
     return;
   }
   
@@ -70,61 +70,64 @@ struct KdTree {
   std::vector<int> search(std::vector<float> target, float distanceTol)
   {
     std::vector<int> ids;
-
+    
     // Initialize
     Node* ptNode = root;
     float tx = target[0];
     float ty = target[1];
     
-    searchHelper (root, target,  distanceTol);
-      
+    uint counter = 0;
+    searchHelper (root, target,  distanceTol, counter);
+    std::cout << " Count stands at " << counter << std::endl;
+    
     return ids;
   }
-
+  
   // ONLY print out results
-  void searchHelper (Node* ptNode, std::vector<float> target, float dTol)
+  void searchHelper (Node* ptNode, std::vector<float> target, float dTol, uint& counter)
   {
-
+    ++counter;
+      
     // SUPER important
     if ( ptNode == nullptr) {
       return;
     }
-
+    
     // Initialize
     float tx = target[0];
     float ty = target[1];
     
     if ( ptNode->point[0] - tx > dTol ) {
       // We are right of target -> go smaller/left
-      searchHelper (ptNode->left, target, dTol);
-
+      searchHelper (ptNode->left, target, dTol, counter);
+      
     } else if ( tx - ptNode->point[0] > dTol ) {
       // We are left of target -> go bigger/right
-      searchHelper (ptNode->right, target, dTol);
-
+      searchHelper (ptNode->right, target, dTol, counter);
+      
     } else {
       // We are W/I x-range
       //
       if ( ptNode->point[1] - ty > dTol) {
-	// We are above target -> go smaller/left
-	searchHelper (ptNode->left, target, dTol);
-
+        // We are above target -> go smaller/left
+        searchHelper (ptNode->left, target, dTol, counter);
+        
       } else if ( ty - ptNode->point[1] > dTol ) {
-	// We are below target -> go bigger/right
-	searchHelper (ptNode->right, target, dTol);
-	
+        // We are below target -> go bigger/right
+        searchHelper (ptNode->right, target, dTol, counter);
+        
       } else {
-	// We are INSIDE box
-	// if ||target - point|| < dTol; add index to vector
-	// Go left
-	// Go right also
-	std::cout << " Got one id=" << ptNode->id << std::endl;
-
-	searchHelper (ptNode->right, target, dTol);
-	searchHelper (ptNode->left, target, dTol);
+        // We are INSIDE box
+        // if ||target - point|| < dTol; add index to vector
+        // Go left
+        // Go right also
+        std::cout << " Got one id=" << ptNode->id << std::endl;
+        
+        searchHelper (ptNode->right, target, dTol, counter);
+        searchHelper (ptNode->left, target, dTol, counter);
       }
     }
-
+    
     return;
   }
 };
