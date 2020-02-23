1e1adbe600c8ef3949b2ea652093229b15134b91
Kt Gunn <ktgunn@hotmail.com>
2020-02-23 18:26:30 -0500

    eigenv.d using templated pca now OK

--- eigenv.d/main.cpp
+++ eigenv.d/main.cpp
@@ -2,26 +2,6 @@
 #include "pca.h"
 
 
-/*
-  #include <iostream>
-  #include <pcl/point_types.h>
-
-  #include <pcl/visualization/pcl_visualizer.h>
-  #include <pcl/common/common.h>
-  #include <pcl/common/transforms.h>
-  #include <pcl/common/pca.h>
-
-
-  // We'll borrow this from Aron Brown for test/debug
-  //
-  struct BoxQ {
-    Eigen::Vector3f bboxTransform;
-    Eigen::Quaternionf bboxQuaternion;
-    float cube_length, cube_width, cube_height; 
-  };
-*/
-
-
  //*****************************************************************************
  // CYLINDERCLOUD
  //   Function to create a point cloud
@@ -56,67 +36,67 @@
 
 
 void TestStuff () {
-  
-   // Create a cloud
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pC = CylinderCloud (1.0, .10, 500);
-  
-  
-  if (false) {
-    // ************** THIS WORKS  -- 'Ptr' pointer, on heap
-    // Create a viewer
-    pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("Viewer"));
-  }
-  
-  // ************** THIS WORKS  ALSO!  -- non-Ptr, on stack
-  // Create a viewer
-  pcl::visualization::PCLVisualizer viewer ("Viewer");
-  
-  
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // Original cloud
-  //
-  viewer.addPointCloud<pcl::PointXYZ> (pC, "Cyl");
-  viewer.addCoordinateSystem (1.0);
-  
-  std::vector<pcl::PointXYZ> vB = k_BoundingBox (pC);
-  viewer.addCube (vB[0].x, vB[1].x, vB[0].y, vB[1].y, vB[0].z, vB[1].z,50,75,33,"Orig");
-  
-  
-  //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-  // Transformed cloud
-  //
-  // Create a transformation
-  const float PI = atan2(0,-1);
-  float tr[3] = {1.0, 0.5, -0.2};
-  float ro[3] = {PI/4, 0.0, 0.0};
-  Eigen::Affine3f XF = k_Compose (tr,ro);
-  
-  // Create a transformed cloud
-  pcl::PointCloud<pcl::PointXYZ>::Ptr pCxF = k_transformCLoud (pC, XF);
-  viewer.addPointCloud<pcl::PointXYZ> (pCxF, "xfCyl");
-  
-  if (false) {
-    // This creates a WORLD centered box instead of a CLOUD centered box
-    vB.empty();
-    vB = k_BoundingBox (pCxF);
-    viewer.addCube (vB[0].x, vB[1].x, vB[0].y, vB[1].y, vB[0].z, vB[1].z,50,75,33, "xF");
     
-  } else {
-    // Correctly sized & oriented bounding box
-    BoxQ qB = k_PCA (pCxF);
+    // Create a cloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pC = CylinderCloud (1.0, .10, 500);
     
-    // pos = (x,y,z) of the MIDDLE of the cube
-    // quaT - quaternion (constructed from two instances of the x-axis vector
-    // xW,yH,zD = overall dimensions (not half)
-    viewer.addCube (qB.bboxTransform, qB.bboxQuaternion, qB.cube_length, qB.cube_width, qB.cube_height , "Foo"); 
-  }
-  
-  if (true) {
-    while (!viewer.wasStopped()) {
-      viewer.spinOnce ();
+    if (false) {
+	// ************** THIS WORKS  -- 'Ptr' pointer, on heap
+	// Create a viewer
+	pcl::visualization::PCLVisualizer::Ptr viewer ( new pcl::visualization::PCLVisualizer ("Viewer"));
     }
-  }
-  
+    
+    // ************** THIS WORKS  ALSO!  -- non-Ptr, on stack
+    // Create a viewer
+    pcl::visualization::PCLVisualizer viewer ("Viewer");
+    
+    
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Original cloud
+    //
+    viewer.addPointCloud<pcl::PointXYZ> (pC, "Cyl");
+    viewer.addCoordinateSystem (1.0);
+    
+    BoxQ qB = k_SimpleBoxQ<typename pcl::PointXYZ>(pC);
+    viewer.addCube (qB.bboxTransform, qB.bboxQuaternion, qB.cube_length, qB.cube_width, qB.cube_height , "Foo"); 
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    // Transformed cloud
+    //
+    // Create a transformation
+    const float PI = atan2(0,-1);
+
+    float tr[3] = {1.0, 0.5, -0.2};
+    float ro[3] = {0.0, 0.0, 0.0};
+
+    if (false) {
+	ro[0] = -1*PI/4;
+    } else {
+	ro[0] = PI/8;
+    }
+    Eigen::Affine3f XF = k_Compose (tr,ro);
+    
+    // Create a transformed cloud
+    pcl::PointCloud<pcl::PointXYZ>::Ptr pCxF = k_transformCloud<pcl::PointXYZ>(pC, XF);
+    viewer.addPointCloud<pcl::PointXYZ> (pCxF, "xfCyl");
+    
+
+    if (false) {
+	BoxQ qBr = k_SimpleBoxQ<pcl::PointXYZ> (pCxF);
+	viewer.addCube (qBr.bboxTransform, qBr.bboxQuaternion, qBr.cube_length, qBr.cube_width, qBr.cube_height , "Foo_R"); 
+    } else {
+	BoxQ qBr2 = k_PCA<pcl::PointXYZ> (pCxF);
+	viewer.addCube (qBr2.bboxTransform, qBr2.bboxQuaternion, qBr2.cube_length, qBr2.cube_width, qBr2.cube_height , "Foo_Real"); 
+    }
+
+    /*    
+    */
+    
+    if (true) {
+	while (!viewer.wasStopped()) {
+	    viewer.spinOnce ();
+	}
+    }
 }
 
 //////////////////////////////////////////////////////////////////////////////////
